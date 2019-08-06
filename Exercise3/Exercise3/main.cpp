402521e5bd5f515d045e8755241f31a7b0d0fbf1
Czeska <cz>
2019-08-06 22:34:20 +0800

    added Exercise3 git commit -m added

---
+++ Exercise3/Exercise3/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  Exercise3
+//
+//  Created by Joan Campos on 06/08/2019.
+//  Copyright © 2019 Czeska Yvonne. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << " Find Size of fundamental data types: " << endl;
+    cout << "--------------------------------------" << endl;
+    cout << " The sizeof(char) is : " << sizeof(char) << " byte " << endl;
+    cout << " The sizeof(short) is : " << sizeof(short) << " bytes " << endl;
+    cout << " The sizeof(int) is : " << sizeof(int) << " bytes " << endl;
+    cout << " The sizeof(long) is : " << sizeof(long) << " bytes " << endl;
+    cout << " The sizeof(float) is : " << sizeof(float) << " bytes " << endl;
+    cout << " The sizeof(double)is : " << sizeof(double) << " bytes " << endl;
+    cout << " The sizeof(long double)is : " << sizeof(long double) << " bytes " << endl;
+    cout << " The sizeof(short)is : " << sizeof(short) << " bytes " << endl;
+    cout << " The sizeof(bool)is : " << sizeof(bool) << " byte " << endl;
+    return 0;
+}
