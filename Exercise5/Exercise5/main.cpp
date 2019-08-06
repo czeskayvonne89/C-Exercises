25e80ff6cabd0423ba363cb4cb58d701cd27e11f
Czeska <cz>
2019-08-06 23:03:40 +0800

    added Exercise5

---
+++ Exercise5/Exercise5/main.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  Exercise5
+//
+//  Created by Joan Campos on 06/08/2019.
+//  Copyright © 2019 Czeska Yvonne. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Check the upper and lower limits of integer : " << endl;
+    cout << "--------------------------------------------- " << endl;
+    cout << "The maximum limit of int data type: "<< INT_MAX << endl;
+    cout << "The maximum limit of int data type: "<< INT_MIN << endl;
+    cout << "The maximum limit of unsigned int data type:  "<< UINT_MAX << endl;
+    cout << "The maximum limit of long long data type:  "<< LLONG_MAX << endl;
+    cout << "The minimum limit of long long data type:  "<< LLONG_MIN << endl;
+    cout << "The maximum limit of unsigned long long data type: "<< ULLONG_MAX << endl;
+    cout << "The Bits contain in char data type : "<< CHAR_BIT << endl;
+    cout << "The maximum limit of char data type : "<< CHAR_MAX << endl;
+    cout << "The minimum limit of char data type : "<< CHAR_MIN << endl;
+    cout << "The maximum limit of signed char data type : "<< SCHAR_MAX << endl;
+    cout << "The minimum limit of signed char data type : "<< SCHAR_MIN << endl;
+    cout << "The maximum limit of unsigned char data type : "<< UCHAR_MAX << endl;
+    cout << "The minimum limit of short data type : "<< SHRT_MIN << endl;
+    cout << "The maximum limit of short data type : "<< SHRT_MAX << endl;
+    cout << "The maximum limit of unsigned short data type : "<< USHRT_MAX << endl;
+    
+    
+    return 0;
+    
+}
