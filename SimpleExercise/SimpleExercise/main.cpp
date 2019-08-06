38f2b6dd2608f28688dd5ccbdfa08de56918f84c
Czeska <cz>
2019-08-06 21:57:16 +0800

    Added exercise 1

---
+++ SimpleExercise/SimpleExercise/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SimpleExercise
+//
+//  Created by Joan Campos on 06/08/2019.
+//  Copyright © 2019 Czeska Yvonne. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
