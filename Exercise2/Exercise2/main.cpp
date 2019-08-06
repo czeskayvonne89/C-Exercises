05f1ca861541286017c940d5f485e258431a519a
Czeska <cz>
2019-08-06 22:13:03 +0800

    added Exercise2

---
+++ Exercise2/Exercise2/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Exercise2
+//
+//  Created by Joan Campos on 06/08/2019.
+//  Copyright © 2019 Czeska Yvonne. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "The sum of 29 and 30 is " << 29 + 30 << endl;
+    return 0;
+}
---
+++ SimpleExercise/SimpleExercise/exer2.cpp
@@ -0,0 +1,16 @@
+//
+//  exer2.cpp
+//  SimpleExercise
+//
+//  Created by Joan Campos on 06/08/2019.
+//  Copyright © 2019 Czeska Yvonne. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World2!\n";
+    return 0;
+}
+
