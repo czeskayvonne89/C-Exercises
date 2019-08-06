98681e3a2420ded014962468fd1015f09085246b
Czeska <cz>
2019-08-06 22:47:45 +0800

    added Exercise4

---
+++ Exercise4/Exercise4/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  Exercise4
+//
+//  Created by Joan Campos on 06/08/2019.
+//  Copyright © 2019 Czeska Yvonne. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Print the sum of two numbers:" << endl;
+    cout << "-----------------------------" << endl;
+    
+    int num1;
+    int num2;
+    int sum;
+    
+    
+    num1 = 29;
+    num2 = 30;
+    sum = num1+num2;
+    
+    cout <<"The sum of " <<num1 << " and " << num2 << " is : " << sum << endl;
+    
+    return 0;
+}
